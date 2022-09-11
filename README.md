4cbc5a6899a4e60b018f62723a6ce7a8adf7536e
Sunglok Choi <sunglok@hanmail.net>
2022-09-12 07:36:27 +0900

    Add 'camera_calib.py' for camera calibration

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 ## SensorPy
-_SensorPy_ is a collection of sensor wrappers written in Python. It does not aim to include every sensors, but to provide convenient (easy-to-use) interfaces for sensors used in [MINT Lab](https://mint-lab.github.io/). The sensor interface was inspired from OpenCV, `cv::VideoCapture`.
+_SensorPy_ is a collection of sensor wrappers written in Python. It does not aim to include every sensor but to provide convenient (easy-to-use) interfaces for sensors used in [MINT Lab](https://mint-lab.github.io/). The sensor interface was inspired by OpenCV `cv::VideoCapture`.
 
 
 
 ### Authors
-* [Sunglok Choi](https://mint-lab.github.io/sunglok/)
-* [Jun Hyeok Choi](https://github.com/cjh1995-ros)
-* [Hyunkil Hwang](https://github.com/Hyunkil76)
+* [Sunglok Choi](https://mint-lab.github.io/sunglok/), [Jun Hyeok Choi](https://github.com/cjh1995-ros), [Hyunkil Hwang](https://github.com/Hyunkil76)
---
+++ camera_calib.py
@@ -0,0 +1,128 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
+import sys, json, glob, math
+
+def get_default_config():
+    config = {
+        'cam_file'          : '',
+        'cam_K'             : None,
+        'cam_dist'          : None,
+        'chess_pattern_cols': 9,
+        'chess_pattern_rows': 6,
+        'chess_cell_size'   : 0.03, # [m]
+        'subpx_winsize'     : 11,
+        'subpx_max_iter'    : 100,
+        'subpx_eps'         : 0.001,
+        'calib_flags'       : [],
+        'calib_output'      : '',
+        'save_cam_pose'     : False,
+        'img_plot_rows'     : 3,
+    }
+    return config
+
+def get_calib_flags(config_param):
+    calib_flags = 0
+    if 'CALIB_USE_INTRINSIC_GUESS'  in config_param: calib_flags += cv.CALIB_USE_INTRINSIC_GUESS
+    if 'CALIB_FIX_PRINCIPAL_POINT'  in config_param: calib_flags += cv.CALIB_FIX_PRINCIPAL_POINT
+    if 'CALIB_FIX_ASPECT_RATIO'     in config_param: calib_flags += cv.CALIB_FIX_ASPECT_RATIO
+    if 'CALIB_ZERO_TANGENT_DIST'    in config_param: calib_flags += cv.CALIB_ZERO_TANGENT_DIST
+    if 'CALIB_FIX_FOCAL_LENGTH'     in config_param: calib_flags += cv.CALIB_FIX_FOCAL_LENGTH
+    if 'CALIB_FIX_K1'               in config_param: calib_flags += cv.CALIB_FIX_K1
+    if 'CALIB_FIX_K2'               in config_param: calib_flags += cv.CALIB_FIX_K2
+    if 'CALIB_FIX_K3'               in config_param: calib_flags += cv.CALIB_FIX_K3
+    if 'CALIB_FIX_K4'               in config_param: calib_flags += cv.CALIB_FIX_K4
+    if 'CALIB_FIX_K5'               in config_param: calib_flags += cv.CALIB_FIX_K5
+    if 'CALIB_FIX_K6'               in config_param: calib_flags += cv.CALIB_FIX_K6
+    if 'CALIB_RATIONAL_MODEL'       in config_param: calib_flags += cv.CALIB_RATIONAL_MODEL
+    if 'CALIB_THIN_PRISM_MODEL'     in config_param: calib_flags += cv.CALIB_THIN_PRISM_MODEL
+    if 'CALIB_FIX_S1_S2_S3_S4'      in config_param: calib_flags += cv.CALIB_FIX_S1_S2_S3_S4
+    if 'CALIB_TILTED_MODEL'         in config_param: calib_flags += cv.CALIB_TILTED_MODEL
+    if 'CALIB_FIX_TAUX_TAUY'        in config_param: calib_flags += cv.CALIB_FIX_TAUX_TAUY
+    return calib_flags
+
+def get_2d_points(given_files, chess_pattern_rows, chess_pattern_cols, subpx_winsize, subpx_criteria):
+    cam_pts, cam_img, cam_files = [], [], []
+    for file in given_files:
+        # Load an image
+        img = cv.imread(file)
+        if len(img.shape) >= 3 and img.shape[2] == 3:
+            img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+
+        # Extract corner points from the images
+        ret, pts = cv.findChessboardCorners(img, (chess_pattern_cols, chess_pattern_rows))
+        if ret:
+            pts = cv.cornerSubPix(img, pts, subpx_winsize, (-1, -1), subpx_criteria)
+            cam_pts.append(pts)
+            cam_img.append(img)
+            cam_files.append(file)
+    return cam_pts, cam_img, cam_files
+
+def get_3d_points(chess_pattern_rows, chess_pattern_cols, chess_cell_size):
+    x, y = np.meshgrid(range(chess_pattern_cols), range(chess_pattern_rows))
+    z = np.zeros_like(x)
+    pts = chess_cell_size * np.dstack((x.reshape(-1, 1), y.reshape(-1, 1), z.reshape(-1, 1)))
+    return pts
+
+def show_reproject_pts(cam_img, cam_K, cam_dist, cam_rvec, cam_tvec, cam_pts, obj_pts, n_rows):
+    if n_rows > 0:
+        plt.figure()
+        for i in range(len(cam_img)):
+            ax = plt.subplot(n_rows, math.ceil(len(cam_img)/n_rows), i + 1)
+            ax.imshow(cam_img[i], cmap='gray')
+            ax.plot(cam_pts[i][:,:,0], cam_pts[i][:,:,1], 'r+', label='extract')
+            proj, _ = cv.projectPoints(obj_pts[i], cam_rvec[i], cam_tvec[i], cam_K, cam_dist)
+            ax.plot(proj[:,:,0], proj[:,:,1], 'b+', label='project')
+            ax.axis('off')
+        plt.tight_layout(pad=0, h_pad=0, w_pad=0)
+
+
+
+if __name__ == '__main__':
+    config_file = 'cv_calib_mono.json'
+    if len(sys.argv) > 1:
+        config_file = sys.argv[1]
+
+    # Load configuration
+    config = get_default_config()
+    with open(config_file, 'rt') as f:
+        config_new = json.load(f)
+        for key, value in config_new.items():
+            if key in config: config[key] = value
+    if not config['cam_file']:
+        raise('An image sequence for the camera is not given')
+    all_files = sorted(glob.glob(config['cam_file']))
+    subpx_winsize = (config['subpx_winsize'], config['subpx_winsize'])
+    subpx_criteria = [cv.TERM_CRITERIA_MAX_ITER + cv.TERM_CRITERIA_EPS, config['subpx_max_iter'], config['subpx_eps']]
+    calib_flags = get_calib_flags(config['calib_flags'])
+
+    # Extract 2D points on the given images
+    cam_pts, cam_img, cam_files = get_2d_points(all_files, config['chess_pattern_rows'], config['chess_pattern_cols'], subpx_winsize, subpx_criteria)
+
+    # Prepare 3D points
+    chessboard = get_3d_points(config['chess_pattern_rows'], config['chess_pattern_cols'], config['chess_cell_size'])
+    obj_pts = [chessboard.astype(np.float32)] * len(cam_img)
+
+    # Calibrate the camera
+    cam_rms, cam_K, cam_dist, cam_rvec, cam_tvec = cv.calibrateCamera(obj_pts, cam_pts, cam_img[0].shape[::-1], config['cam_K'], config['cam_dist'], flags=calib_flags)
+
+    # Write the calibration result
+    if config['calib_output']:
+        with open(config['calib_output'], 'wt') as f:
+            calib_result = {'cam_K': cam_K.tolist(), 'cam_dist': cam_dist.tolist()}
+            if config['save_cam_pose']:
+                calib_result['cam_pose'] = []
+                for idx, file in enumerate(cam_files):
+                    calib_result['cam_pose'].append({'file': file, 'rvec': cam_rvec[idx].tolist(), 'tvec': cam_tvec[idx].tolist()})
+            json.dump(calib_result, f, indent=4)
+
+    # Print the calibration result briefly
+    print('### Brief Calibration Report')
+    print(f'* Calibration flags: {bin(calib_flags)}')
+    print(f'* Camera files: {config["cam_file"]}')
+    print(f'* The number of used images: {len(cam_files)} / {len(all_files)}')
+    print(f'* RMS error: {cam_rms:.6f}')
+
+    # Visualize reprojected points
+    show_reproject_pts(cam_img, cam_K, cam_dist, cam_rvec, cam_tvec, cam_pts, obj_pts, config['img_plot_rows'])
+    plt.show()
