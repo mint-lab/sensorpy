a351e0d64e99e26ea62630a774a0f2cf647e1479
Sunglok Choi <sunglok@hanmail.net>
2023-01-14 07:44:59 +0900

    Rename '_dist' as '_distort' (due to its ambiguity)

--- camera_calib.py
+++ camera_calib.py
@@ -7,7 +7,7 @@ def get_default_config():
     config = {
         'cam_file'          : '',
         'cam_K'             : None,
-        'cam_dist'          : None,
+        'cam_distort'       : None,
         'chess_pattern_cols': 9,
         'chess_pattern_rows': 6,
         'chess_cellsize'    : 0.03, # [m]
@@ -49,8 +49,8 @@ def load_config(config_file):
 
     if type(config['cam_K']) is list:
         config['cam_K'] = np.array(config['cam_K'])
-    if type(config['cam_dist']) is list:
-        config['cam_dist'] = np.array(config['cam_dist'])
+    if type(config['cam_distort']) is list:
+        config['cam_distort'] = np.array(config['cam_distort'])
     if (type(config['subpx_winsize']) is not tuple) or (type(config['subpx_winsize']) is not list):
         config['subpx_winsize'] = (config['subpx_winsize'], config['subpx_winsize'])
     config['subpx_criteria'] = [cv.TERM_CRITERIA_MAX_ITER + cv.TERM_CRITERIA_EPS, config['subpx_max_iter'], config['subpx_eps']]
@@ -80,14 +80,14 @@ def get_3d_points(chess_pattern_rows, chess_pattern_cols, chess_cellsize):
     pts = chess_cellsize * np.dstack((x.reshape(-1, 1), y.reshape(-1, 1), z.reshape(-1, 1)))
     return pts
 
-def show_reproject_pts(cam_img, cam_K, cam_dist, cam_rvec, cam_tvec, cam_pts, obj_pts, n_rows):
+def show_reproject_pts(cam_img, cam_K, cam_distort, cam_rvec, cam_tvec, cam_pts, obj_pts, n_rows):
     if n_rows > 0:
         plt.figure()
         for i in range(len(cam_img)):
             ax = plt.subplot(n_rows, math.ceil(len(cam_img)/n_rows), i + 1)
             ax.imshow(cam_img[i], cmap='gray')
             ax.plot(cam_pts[i][:,:,0], cam_pts[i][:,:,1], 'r+', label='extract')
-            proj, _ = cv.projectPoints(obj_pts[i], cam_rvec[i], cam_tvec[i], cam_K, cam_dist)
+            proj, _ = cv.projectPoints(obj_pts[i], cam_rvec[i], cam_tvec[i], cam_K, cam_distort)
             ax.plot(proj[:,:,0], proj[:,:,1], 'b+', label='project')
             ax.axis('off')
         plt.tight_layout(pad=0, h_pad=0, w_pad=0)
@@ -95,7 +95,7 @@ def show_reproject_pts(cam_img, cam_K, cam_dist, cam_rvec, cam_tvec, cam_pts, ob
 
 
 if __name__ == '__main__':
-    config_file = 'cv_calib_mono.json'
+    config_file = 'camera_calib.json'
     if len(sys.argv) > 1:
         config_file = sys.argv[1]
 
@@ -113,12 +113,12 @@ if __name__ == '__main__':
     obj_pts = [chessboard.astype(np.float32)] * len(cam_img)
 
     # Calibrate the camera
-    cam_rms, cam_K, cam_dist, cam_rvec, cam_tvec = cv.calibrateCamera(obj_pts, cam_pts, cam_img[0].shape[::-1], config['cam_K'], config['cam_dist'], flags=config['calib_flags'])
+    cam_rms, cam_K, cam_distort, cam_rvec, cam_tvec = cv.calibrateCamera(obj_pts, cam_pts, cam_img[0].shape[::-1], config['cam_K'], config['cam_distort'], flags=config['calib_flags'])
 
     # Write the calibration result
     if config['calib_output']:
         with open(config['calib_output'], 'wt') as f:
-            calib_result = {'cam_K': cam_K.tolist(), 'cam_dist': cam_dist.tolist()}
+            calib_result = {'cam_K': cam_K.tolist(), 'cam_distort': cam_distort.tolist()}
             if config['save_cam_pose']:
                 calib_result['cam_pose'] = []
                 for idx, file in enumerate(cam_files):
@@ -133,5 +133,5 @@ if __name__ == '__main__':
     print(f'* RMS error: {cam_rms:.6f} [pixel]')
 
     # Visualize reprojected points
-    show_reproject_pts(cam_img, cam_K, cam_dist, cam_rvec, cam_tvec, cam_pts, obj_pts, config['img_plot_rows'])
+    show_reproject_pts(cam_img, cam_K, cam_distort, cam_rvec, cam_tvec, cam_pts, obj_pts, config['img_plot_rows'])
     plt.show()
